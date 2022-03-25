a482aa84c5762540a2d23b2868a6e4eef937d2e1
xiaozhihonghong <m13127979789@163.com>
2022-03-25 16:45:54 +0800

    20220225_group

--- geeWeb/gee/gee.go
+++ geeWeb/gee/gee.go
@@ -6,29 +6,94 @@ import (
 
 type handleFunc func(*Context)
 
+//type Engine struct {
+//	prefix string
+//	middlerwares []handleFunc
+//	router *router
+//}
+//
+//func NewEngine() *Engine {
+//	engine := &Engine{middlerwares: make([]handleFunc, 0), router: newRouter()}
+//	return engine
+//}
+//
+//func (e *Engine) Group(prefix string) *Engine {
+//	newGroup := &Engine{
+//		prefix: e.prefix + prefix,
+//		router: e.router,
+//	}
+//	return newGroup
+//}
+//
+////添加路由表,key是GET-/或GET-/hello这种形式，方法+-+path形式
+//func (e *Engine) AddRouter(method string, comp string, handle handleFunc) {
+//	pattern := e.prefix + comp
+//	e.router.AddRouter(method, pattern, handle)
+//}
+//
+////Get,将方法映射到路由表中，通过run方法运行
+//func (e *Engine) GET(patter string, handle handleFunc)  {
+//	e.AddRouter("GET", patter, handle)
+//}
+//
+////Post
+//func (e *Engine) POST(patter string, handle handleFunc)  {
+//	e.AddRouter("POST", patter, handle)
+//}
+
+
+// gin的分布控制方式
+type RouterGroup struct {
+	prefix string
+	middlerwares []handleFunc
+	parent *RouterGroup
+	engine *Engine
+}
+
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup
 }
 
 func NewEngine() *Engine {
-	return &Engine{
-		router: newRouter(),
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{
+		//middlerwares: make([]handleFunc, 0),
+		engine: engine,
 	}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+func (r *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := r.engine
+	newGroup := &RouterGroup{
+		prefix: r.prefix + prefix,
+		parent: r,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
 }
 
 //添加路由表,key是GET-/或GET-/hello这种形式，方法+-+path形式
-func (e *Engine) AddRouter(method string, pattern string, handle handleFunc) {
-	e.router.AddRouter(method, pattern, handle)
+func (r *RouterGroup) AddRouter(method string, comp string, handle handleFunc) {
+	pattern := r.prefix + comp
+	r.engine.router.AddRouter(method, pattern, handle)
 }
 
 //Get,将方法映射到路由表中，通过run方法运行
-func (e *Engine) GET(patter string, handle handleFunc)  {
-	e.AddRouter("GET", patter, handle)
+func (r *RouterGroup) GET(patter string, handle handleFunc)  {
+	r.engine.AddRouter("GET", patter, handle)
+	r.AddRouter("GET", patter, handle)        //之前一直运行结果为404，由于使用的上面的语句，单只每次从都是从engine中调用add，
+														// 所以一直都是无r.prefix + comp的状态
 }
 
 //Post
-func (e *Engine) POST(patter string, handle handleFunc)  {
-	e.AddRouter("POST", patter, handle)
+func (r *RouterGroup) POST(patter string, handle handleFunc)  {
+	//r.engine.AddRouter("POST", patter, handle)
+	r.AddRouter("POST", patter, handle)
 }
 
 //run
--- geeWeb/main.go
+++ geeWeb/main.go
@@ -8,23 +8,31 @@ import (
 
 func main() {
 	r := gee.NewEngine()
-	r.GET("/", func(c *gee.Context) {
+	r.GET("/index", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	})
+	v1 := r.Group("/v1")
+	v1.GET("/", func(c *gee.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
-	r.GET("/hello", func(c *gee.Context) {
+	v1.GET("/hello", func(c *gee.Context) {
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
-	r.GET("/hello/:name", func(c *gee.Context) {
+	v1.GET("/hello/:name", func(c *gee.Context) {
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
 	})
-	r.GET("/assert/*filepath", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{"filepath":c.Param("filepath")})
+	v2 := r.Group("/v2")
+	v2.GET("/assert/*filepath", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
 	})
-	//r.POST("/", func(c *gee.Context) {
-	//	c.JSON(http.StatusOK, gee.H{
-	//		"username": c.PostForm("username"),
-	//		"password": c.PostForm("password"),
-	//	})
-	//})
+	v2.GET("/hello/:name", func(c *gee.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+	v2.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
 	r.Run(":9999")
 }
