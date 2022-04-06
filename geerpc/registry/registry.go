7833c15b129f9b5da006f32255e20297b6f9a5cd
xiaozhihonghong <m13127979789@163.com>
2022-04-06 19:40:45 +0800

    20220406_register

---
+++ geerpc/registry/registry.go
@@ -0,0 +1,124 @@
+package registry
+
+import (
+	"log"
+	"net/http"
+	"sort"
+	"strings"
+	"sync"
+	"time"
+)
+
+//实例
+type ServerItem struct {
+	Addr  string
+	start time.Time  //注册的服务超过5分钟为不可用
+}
+
+//所有实例的仓库
+type GeeRegistry struct {
+	timeout time.Duration
+	mu      sync.Mutex
+	servers map[string]*ServerItem
+}
+
+const (
+	defaultPath    = "/_geerpc_/registry"
+	defaultTimeout = time.Minute * 5
+)
+
+// New create a registry instance with timeout setting
+func New(timeout time.Duration) *GeeRegistry {
+	return &GeeRegistry{
+		servers: make(map[string]*ServerItem),
+		timeout: timeout,
+	}
+}
+
+var DefaultGeeRegister = New(defaultTimeout)
+
+//添加实例，如果存在，则更新
+func (r *GeeRegistry) putServer(addr string) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	s := r.servers[addr]
+	if s == nil {
+		r.servers[addr] = &ServerItem{Addr: addr, start: time.Now()}
+	} else {
+		s.start = time.Now()
+	}
+}
+
+//返回所有的可用服务，如果服务超时，则删除
+func (r *GeeRegistry) aliveServers() []string {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	var alive []string
+	for addr, s := range r.servers {
+		if r.timeout == 0 || s.start.Add(r.timeout).After(time.Now()) {
+			alive = append(alive, addr)
+		} else {
+			delete(r.servers, addr)
+		}
+	}
+	sort.Strings(alive)
+	return alive
+}
+
+//Get返回所有的服务列表，Post添加服务实例，所有信息都放到header中
+func (r *GeeRegistry) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "GET":
+		// keep it simple, server is in req.Header
+		w.Header().Set("X-Geerpc-Servers", strings.Join(r.aliveServers(), ","))
+	case "POST":
+		// keep it simple, server is in req.Header
+		addr := req.Header.Get("X-Geerpc-Server")
+		if addr == "" {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		r.putServer(addr)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func (r *GeeRegistry) HandleHTTP(registryPath string) {
+	http.Handle(registryPath, r)
+	log.Println("rpc registry path:", registryPath)
+}
+
+func HandleHTTP() {
+	DefaultGeeRegister.HandleHTTP(defaultPath)
+}
+
+//定期向注册中心发送心跳
+func Heartbeat(registry, addr string, duration time.Duration) {
+	if duration == 0 {
+		// make sure there is enough time to send heart beat
+		// before it's removed from registry
+		duration = defaultTimeout - time.Duration(1)*time.Minute
+	}
+	var err error
+	err = sendHeartbeat(registry, addr)
+	go func() {
+		t := time.NewTicker(duration)
+		for err == nil {
+			<-t.C
+			err = sendHeartbeat(registry, addr)
+		}
+	}()
+}
+
+func sendHeartbeat(registry, addr string) error {
+	log.Println(addr, "send heart beat to registry", registry)
+	httpClient := &http.Client{}
+	req, _ := http.NewRequest("POST", registry, nil)
+	req.Header.Set("X-Geerpc-Server", addr)
+	if _, err := httpClient.Do(req); err != nil {
+		log.Println("rpc server: heart beat err:", err)
+		return err
+	}
+	return nil
+}
